b7061bb15e4f1edd698d2d672e4d99050c6c1e99
remix <http+github.com@remix.asia>
2021-06-20 11:09:57 +0900

    onRequestPermissionsResult, WRITE_EXTERNAL_STORAGE

--- app/src/main/java/asia/remix/myapplication/MainActivity.java
+++ app/src/main/java/asia/remix/myapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package asia.remix.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
+	static final int STORAGE_REQUEST_CODE = 102;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ){
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
+
+		if( ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED ){
+			ActivityCompat.requestPermissions( this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_REQUEST_CODE );
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult( int requestCode, String[] permissions, int[] grantResults ){
+		Log.d( "■", "onRequestPermissionsResult()" );
+		super.onRequestPermissionsResult( requestCode, permissions, grantResults );
+		if( requestCode == STORAGE_REQUEST_CODE ){
+			if( grantResults[0] != PackageManager.PERMISSION_GRANTED ){
+				Toast.makeText( this, "must WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT ).show();
+				finish();
+			}
+		}
 	}
 }
