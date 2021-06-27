1481a3afe6f82c380795f99974406dbe884e7db6
remix <http+github.com@remix.asia>
2021-06-27 15:04:52 +0900

    複数権限時 RequestMultiplePermissions() ← RequestPermission()

--- app/src/main/java/asia/remix/myapplication/MainActivity.java
+++ app/src/main/java/asia/remix/myapplication/MainActivity.java
@@ -12,26 +12,39 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity{
+	static String[] strPermission = new String[]{
+		Manifest.permission.WRITE_EXTERNAL_STORAGE
+	,	Manifest.permission.RECORD_AUDIO
+	};
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ){
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
 
-		if( ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED ){
-			launcher.launch( Manifest.permission.WRITE_EXTERNAL_STORAGE );
+		if( ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED
+		||  ContextCompat.checkSelfPermission( this, Manifest.permission.RECORD_AUDIO ) != PackageManager.PERMISSION_GRANTED
+		){
+			launcher.launch( strPermission );
 		}
 	}
 
-	ActivityResultLauncher<String> launcher = registerForActivityResult(
-		new ActivityResultContracts.RequestPermission()
-	,	new ActivityResultCallback<Boolean>(){
-			public void onActivityResult( Boolean result ){
-				if( result ){
-					Log.d( "■", "onActivityResult()" );
-				}else{
-					Toast.makeText( MainActivity.this, "must WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT ).show();
+	ActivityResultLauncher<String[]> launcher = registerForActivityResult(
+		new ActivityResultContracts.RequestMultiplePermissions()
+	,	new ActivityResultCallback<Map<String, Boolean>>(){
+			public void onActivityResult( Map<String, Boolean> isGranted ){
+				boolean allGranted = true;
+				for( String key : isGranted.keySet() ){
+					Log.d( "■", "onActivityResult()" + key + ":" + isGranted.get( key ) );
+					if( ! isGranted.get( key ) ){
+						allGranted = false;
+					}
+				}
+				if( ! allGranted ){
+					Toast.makeText( MainActivity.this, "must WRITE_EXTERNAL_STORAGE, RECORD_AUDIO", Toast.LENGTH_SHORT ).show();
 					finish();
 				}
 			}
